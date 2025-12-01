1c8ae3d5cdb9d6fd1e3eb66d458fc4aac5f16e63
ashlyaud <maksimknutov90@gmail.com>
2025-12-01 23:50:50 +0300

    Added Json_Storage with Load and Save methods

---
+++ src/storage/json_storage.go
@@ -0,0 +1,69 @@
+package storage
+
+import (
+	"CLI-task-tracker/task"
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type JsonStorage struct {
+	path string
+}
+
+func NewJsonStorage(path string) *JsonStorage {
+	return &JsonStorage{path}
+}
+
+func (js *JsonStorage) Load() ([]*task.Task, error) {
+	file, err := os.Open(js.path)
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			return []*task.Task{}, nil
+		}
+		return nil, err
+	}
+
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(data) == 0 {
+		return []*task.Task{}, nil
+	}
+
+	var tasks []*task.Task
+	
+	err = json.Unmarshal(data, &tasks)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+func (js *JsonStorage) Save(tasks []*task.Task) error {
+	file, err := os.Create(js.path)
+
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	data, err := json.Marshal(tasks)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(data)
+
+	return err
+}
